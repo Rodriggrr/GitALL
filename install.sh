4c6ca2fa0beed46c05397a16b2a9f7150b833148
Rodrigo Farinon <rfarinon@alu.ufc.br>
2023-08-28 03:57:19 -0300

    up

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 #install termocolor dependency
 echo "Installing PIP..."
-sudo apt-get install python3-pip > dev/null 2>&1
-echo "Installed."
+sudo apt-get install python3-pip > /dev/null 2>&1
+echo "Installed or already exists."
 
 echo "Installing python termcolor..."
-pip install termcolor > dev/null 2>&1
-echo "Installed."
+pip install termcolor > /dev/null 2>&1
+echo "Installed or already exists."
 
 wget https://raw.githubusercontent.com/Rodriggrr/GitALL/master/gitall
 chmod +x gitall
